f427893a53a3b43900051b0dc561bde01a694833
Olivia Cubela <oliviacubela@gmail.com>
2020-02-25 04:32:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,7 @@
 def display_board
-  puts "A Tic Tac Toe Board"
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
 puts "-----------"
-puts "   |   |   "#
+puts "   |   |   "
 end
- Define a method display_board that prints a 3x3 Tic Tac Toe Board
